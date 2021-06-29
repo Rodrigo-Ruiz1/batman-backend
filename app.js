83acd6d9c70811bdf9d3dc01410a3fcde8eb7ca0
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-06-29 16:56:13 -0400

    installed express-session

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const express = require('express');
+const session = require('express-session');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+
+app.use(session( {
+    secret: 'get rad!',
+    resave: false,
+    saveUninitialized: false,
+    is_logged_in: false
+}))
 
 const es6Renderer = require('express-es6-template-engine');
 
